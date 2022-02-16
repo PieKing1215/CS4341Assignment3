f33dcff6d7618341578a0e8043bd40a9d3210edb
David M <PieKing1215@users.noreply.github.com>
2022-02-16 13:25:02 -0500

    Pick heuristic from cmd line args

--- src/astar/Heuristics.java
+++ src/astar/Heuristics.java
@@ -1,8 +1,36 @@
 package astar;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalInt;
 
 public class Heuristics {
+
+    public static List<Heuristic> all() {
+        return Arrays.asList(
+                Heuristics::heuristic1,
+                Heuristics::heuristic2,
+                Heuristics::heuristic3,
+                Heuristics::heuristic4,
+                Heuristics::heuristic5,
+                Heuristics::heuristic6,
+                Heuristics::heuristic7
+        );
+    }
+
+    public static int heuristic1(Position currentPos, OptionalInt[] squaresAroundCurrent, Facing facing, Position goalPos, OptionalInt[] squaresAroundGoal) {
+        return 0;
+    }
+
+    public static int heuristic2(Position currentPos, OptionalInt[] squaresAroundCurrent, Facing facing, Position goalPos, OptionalInt[] squaresAroundGoal) {
+        int[] distances = calcDistances(currentPos, goalPos);
+        return Math.min(distances[0], distances[1]);
+    }
+
+    public static int heuristic3(Position currentPos, OptionalInt[] squaresAroundCurrent, Facing facing, Position goalPos, OptionalInt[] squaresAroundGoal) {
+        int[] distances = calcDistances(currentPos, goalPos);
+        return Math.max(distances[0], distances[1]);
+    }
 
     public static int heuristic4(Position currentPos, OptionalInt[] squaresAroundCurrent, Facing facing, Position goalPos, OptionalInt[] squaresAroundGoal) {
         int[] distances = calcDistances(currentPos, goalPos);
@@ -17,6 +45,11 @@ public class Heuristics {
 
     public static int heuristic6(Position currentPos, OptionalInt[] squaresAroundCurrent, Facing facing, Position goalPos, OptionalInt[] squaresAroundGoal) {
         return heuristic5(currentPos, squaresAroundCurrent, facing, goalPos, squaresAroundGoal) * 3;
+    }
+
+    public static int heuristic7(Position currentPos, OptionalInt[] squaresAroundCurrent, Facing facing, Position goalPos, OptionalInt[] squaresAroundGoal) {
+        // TODO
+        return 0;
     }
 
     public static int[] calcDistances(Position pos, Position goalPos) {
